af9f9a3a6d8bae85e1b7cf371d011a9fe8986dbd
Anubhav Ujjawal <anubhavujjawal@gmail.com>
2019-07-31 18:43:09 +0530

    added heroku to allowed hosts in staging_settings

--- fyle_backend/staging_settings.py
+++ fyle_backend/staging_settings.py
@@ -2,6 +2,10 @@ import dj_database_url
 
 from .settings import *
 
+ALLOWED_HOSTS = [
+    'limitless-springs-53595.herokuapp.com'
+]
+
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
