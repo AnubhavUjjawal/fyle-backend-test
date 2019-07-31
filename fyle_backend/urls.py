4699e6efb70442100b43fe3096c3852bf326891e
Anubhav Ujjawal <anubhavujjawal@gmail.com>
2019-07-31 14:15:25 +0530

    added swagger

--- fyle_backend/urls.py
+++ fyle_backend/urls.py
@@ -14,12 +14,35 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from rest_framework import permissions
 from rest_framework_jwt.views import obtain_jwt_token
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="anubhavujjawal@gmail.com"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+
+]
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('rest_registration.api.urls')),
     path('bank/', include('bank_service.urls')),
     path('api-token-auth/', obtain_jwt_token),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
