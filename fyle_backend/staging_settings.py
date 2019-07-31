4c7d6f73ff32dc55e62013638fd458e2266a53af
Anubhav Ujjawal <anubhavujjawal@gmail.com>
2019-07-31 18:36:35 +0530

    added whitenoise

--- fyle_backend/staging_settings.py
+++ fyle_backend/staging_settings.py
@@ -4,3 +4,5 @@ from .settings import *
 
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
